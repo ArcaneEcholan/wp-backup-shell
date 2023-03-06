e5023b70db7fcb5f5c76af4acfb515c3d104b329
wc <2019012617@bistu.edu.cn>
2023-03-06 21:34:44 +0800

    commit

--- backup-blog.sh
+++ backup-blog.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-# 可变参数
-blogname_prefix="blog" 
-containername="wp-nginx"
-blogdirname="blog"
-nginxroot="/var/www/html"
+# editable vars
+blog_archive_name_prefix="blog"
+wp_container_name="wp-nginx"
+web_root_dir="/var/www/html"
 
-sudo docker exec -i  ${containername} /bin/bash << EOF
+sudo docker exec -i ${wp_container_name} /bin/bash <<EOF
 
-cd ${nginxroot}
+cd ${web_root_dir}
 
-tar -cf blog.tar.gz ${blogdirname}
+tar -cPf blog.tar.gz ./*
+
+mkdir /backup-dir
+
+mv blog.tar.gz /backup-dir
 
 exit
 
@@ -18,8 +21,7 @@ EOF
 
 # 备份sql文件名
 time=$(date "+%Y-%m-%d-%H-%M-%S")
-blogname="${blogname_prefix}-${time}.tar.gz"
+blogname="${blog_archive_name_prefix}-${time}.tar.gz"
 echo ${blogname}
 
-sh -c "sudo docker cp ${containername}:${nginxroot}/blog.tar.gz ${blogname}"
-
+sh -c "sudo docker cp ${wp_container_name}:/backup-dir/blog.tar.gz ${blogname}"
